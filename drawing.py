c40ed5e639de1af556f4dd45ce28889522a61c9e
lemonsouffle <105547281+lemonsouffle@users.noreply.github.com>
2023-02-27 15:51:14 +0300

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -3,9 +3,12 @@ import random
 
 turtle.bgcolor('black')
 turtle.speed(100)
+colors = ['red', 'green', 'brown', 'blue', 'white', 'yellow', 'orange', 'gray', 'pink', 'cyan', 'purple']
+color = random.choice(colors)
+turtle.color(color)
 size = random.randint(10, 60)
 for j in range(size):
-    turtle.forward(2*j)
+    turtle.forward(2 * j)
     turtle.left(95)
-    
+
 turtle.mainloop()
