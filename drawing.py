7e2e93b545987bf651f4e6d0e9eb897c7c6e3ea2
lemonsouffle <105547281+lemonsouffle@users.noreply.github.com>
2023-02-27 15:57:42 +0300

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -4,21 +4,24 @@ import random
 
 def spiral(size):
     for j in range(size):
-        turtle.forward(2*j)
+        turtle.forward(2 * j)
         turtle.left(95)
-        
+
 
 turtle.bgcolor('black')
-turtle.speed(100)
+turtle.speed(1000)
 colors = ['red', 'green', 'brown', 'blue', 'white', 'yellow', 'orange', 'gray', 'pink', 'cyan', 'purple']
-color = random.choice(colors)
-turtle.color(color)
-size = random.randint(10, 60)
-x = random.randint(-turtle.window_width()//2, turtle.window_width()//2)
-y = random.randint(-turtle.window_height()//2, turtle.window_height()//2)
-turtle.penup()
-turtle.setpos(x, y)
-turtle.pendown()
-spiral(size)
+
+for i in range(50):
+    turtle.color(random.choice(colors))
+
+    turtle.penup()
+    x = random.randint(-turtle.window_width() // 2, turtle.window_width() // 2)
+    y = random.randint(-turtle.window_height() // 2, turtle.window_height() // 2)
+    turtle.setpos(x, y)
+    turtle.pendown()
+    
+    size = random.randint(10, 60)
+    spiral(size)
 
 turtle.mainloop()
