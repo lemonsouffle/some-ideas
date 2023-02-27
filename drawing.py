02c53dd0a8e235dc00259b7b51a93089dd7d06b5
lemonsouffle <105547281+lemonsouffle@users.noreply.github.com>
2023-02-27 15:52:38 +0300

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -7,6 +7,11 @@ colors = ['red', 'green', 'brown', 'blue', 'white', 'yellow', 'orange', 'gray', 
 color = random.choice(colors)
 turtle.color(color)
 size = random.randint(10, 60)
+x = random.randint(-turtle.window_width()//2, turtle.window_width()//2)
+y = random.randint(-turtle.window_height()//2, turtle.window_height()//2)
+turtle.penup()
+turtle.setpos(x, y)
+turtle.pendown()
 for j in range(size):
     turtle.forward(2 * j)
     turtle.left(95)
